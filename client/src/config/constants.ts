7c54377caec4829fbf4e6266e3b11aa090d72b9c
ya-dvorovenko <yadvorovenko@gmail.com>
2026-02-13 23:04:42 +0100

    update env

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,9 +1,6 @@
 export const PACKAGE_ID =
   "0x815fd83d04fb33778cde254aae345302967c0eb600037e2164bf374f3fbd01db";
 
-export const PUBLISHER_ID =
-  "0x316d478636aec6a4029ad0cbf7cc22650769c154cf9b0ab61737db55fa4e1a15";
-
 export const GRAPHQL_ENDPOINT = "https://graphql.testnet.sui.io/graphql";
 
 export const CLOCK_ID = "0x6";
